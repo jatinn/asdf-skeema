006f69909bc5e1f6425f6b447046b70312da9880
jatinn <jatinn@users.noreply.github.com>
2024-02-28 11:00:35 -0500

    cleanup todos

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for skeema.
 GH_REPO="https://github.com/skeema/skeema"
 TOOL_NAME="skeema"
 TOOL_TEST="skeema --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if skeema has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,7 +38,6 @@ download_release_source() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for skeema
 	url="$GH_REPO/archive/v${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
@@ -62,7 +58,6 @@ download_release_asset() {
 		platform="mac"
 	fi
 
-	# TODO: Adapt the release URL convention for skeema
 	url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}_${version}_${platform}_${arch}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
@@ -82,7 +77,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert skeema executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
